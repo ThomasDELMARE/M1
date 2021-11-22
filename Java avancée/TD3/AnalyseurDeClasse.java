0be30f4fe7f7f03b5c5e2f02daa883388debc458
Thomas DELMARE <71331024+ThomasDELMARE@users.noreply.github.com>
2021-11-22 20:05:24 +0100

    Fin TD3

--- Java avancée/TD3/AnalyseurDeClasse.java
+++ Java avancée/TD3/AnalyseurDeClasse.java
@@ -6,35 +6,30 @@
  */
 
 import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
 import java.lang.reflect.*;
+import java.util.ArrayList;
+
+import miageAdvanced.etat;
 
 import java.io.*;
 
-@miageBasics(
-    nom = "DELMARE",
-    prenom = "Thomas",
-    annee = 2021,
-    module = "Java avancée",
-    numSeance = 3
-  )
+@miageBasics(nom = "DELMARE", prenom = "Thomas", annee = 2020, module = "Java avancée", numSeance = 3)
 public class AnalyseurDeClasse {
 
-  @miageAdvanced(
-    etatCompletude = miageAdvanced.etat.DRAFT_PARTIEL,
-    etatQualite = miageAdvanced.etat.DRAFT_PARTIEL,
-    etatTests = false,
-    etatAutomatisation = false
-  )
+  @miageBasics(nom = "DELMARE", prenom = "Thomas", annee = 2020, module = "Java avancée", numSeance = 3)
+  public AnalyseurDeClasse() {
+  }
+
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_PARTIEL, etatTests = false, etatAutomatisation = false)
   public static void analyseClasse(String nomClasse) throws ClassNotFoundException {
     // Récupération d'un objet de type Class correspondant au nom passé en
     // paramétres
     Class cl = getClasse(nomClasse);
-
     afficherProprietesClasse(cl);
-
   }
 
-  @miageBasics
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.VERSION_FINALISEEE, etatTests = false, etatAutomatisation = false)
   public static void afficherProprietesClasse(Class cl) {
     afficheEnTeteClasse(cl);
 
@@ -55,7 +50,7 @@ public class AnalyseurDeClasse {
   }
 
   /** Retourne la classe dont le nom est passé en paramétre */
-  @miageBasics
+  @miageBasics(nom = "DELMARE", prenom = "Thomas", annee = 2020, module = "Java avancée", numSeance = 1)
   public static Class getClasse(String nomClasse) throws ClassNotFoundException {
     try {
       Class c = Class.forName(nomClasse);
@@ -70,7 +65,7 @@ public class AnalyseurDeClasse {
   /**
    * Cette méthode affiche par ex "public class C1 extends C2 implements I1, I2 {"
    */
-  @miageBasics
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_PARTIEL, etatTests = false, etatAutomatisation = false)
   public static void afficheEnTeteClasse(Class cl) {
     // Affichage du modifier et du nom de la classe
     String finalChar = "";
@@ -112,7 +107,7 @@ public class AnalyseurDeClasse {
    * Cette méthode affiche les classes imbriquées statiques ou pas A faire aprés
    * avoir fait fonctionner le reste
    */
-  @miageAdvanced
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_COMPLET, etatTests = false, etatAutomatisation = false)
   public static void afficheInnerClasses(Class cl) {
     Class[] classes = cl.getDeclaredClasses();
 
@@ -140,7 +135,7 @@ public class AnalyseurDeClasse {
 
   }
 
-  @miageBasics
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_PARTIEL, etatTests = false, etatAutomatisation = false)
   public static void afficheAttributs(Class cl) {
     Field[] result = cl.getDeclaredFields();
 
@@ -167,7 +162,7 @@ public class AnalyseurDeClasse {
     System.out.println("}");
   }
 
-  @miageBasics
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_COMPLET, etatTests = false, etatAutomatisation = false)
   public static void afficheConstructeurs(Class cl) {
     Constructor[] result = cl.getConstructors();
 
@@ -195,7 +190,7 @@ public class AnalyseurDeClasse {
 
   }
 
-  @miageBasics
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_COMPLET, etatTests = true, etatAutomatisation = false)
   public static void afficheMethodes(Class cl) {
     Method[] result = cl.getMethods();
 
@@ -223,7 +218,7 @@ public class AnalyseurDeClasse {
     System.out.println("}");
   }
 
-  @miageBasics
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_PARTIEL, etatTests = false, etatAutomatisation = false)
   public static String getModifier(int modifierInt) {
     String result = "";
 
@@ -258,19 +253,162 @@ public class AnalyseurDeClasse {
     return result;
   }
 
-  /* Facultatif au moins dans un premier temps */
-  /*
-   * tester le programme en passant un nom de classe complet en paramétre Modifier
-   * la méthode "main" en conséquence
-   */
+  // ANNOTATIONS LISTING
 
-  @miageBasics
+  public void listerDonneesAnnotationsBasics(miageBasics[] annotationsList) {
+    this.annotationsAnnees2020(annotationsList);
+    this.champsEtMethodesAnnees2020(annotationsList);
+    this.champsCreesPendantSeance1Et2(annotationsList);
+  }
+
+  public void listerDonneesAnnotationsAdvanced(miageAdvanced[] annotationsList) {
+    this.methodesDe2020NonFinalisees(annotationsList);
+    this.calculerRatios(annotationsList);
+  }
+
+  public void calculerRatios(miageAdvanced[] annotationsList) {
+    this.ratioMethodeFinalisesRealisees(annotationsList, 2020);
+    this.ratioDraftParielMethodesRealisees(annotationsList, 2020);
+    this.ratioMethodesGenereesMethodesRealisees(annotationsList, 2020);
+    this.ratioMethodesFinaliseesMethodesNonGenerees(annotationsList, 2020);
+    this.ratioMethodesTesteesMethodesNonGenerees(annotationsList, 2020);
+  }
+
+  public void annotationsAnnees2020(miageBasics[] annotationsList) {
+    System.out.println("Liste des classes faites en 2020.");
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      // FILTRER SELON LIEN AVEC L ANNOTATION (classe)
+      if (annotationsList[i].annee() == 2020) {
+        System.out.println(annotationsList[i].toString());
+      }
+    }
+  }
+
+  public void champsEtMethodesAnnees2020(miageBasics[] annotationsList) {
+    System.out.println("Liste des champs et méthodes faites en 2020.");
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      // FILTRER SELON LIEN AVEC L ANNOTATION (METHODES, CHAMPS)
+      if (annotationsList[i].annee() == 2020) {
+        System.out.println(annotationsList[i].toString());
+      }
+    }
+  }
+
+  public void champsCreesPendantSeance1Et2(miageBasics[] annotationsList) {
+    System.out.println("Liste des annotations créées pendant la séance 1 et 2.");
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      if (annotationsList[i].numSeance() == 1 || annotationsList[i].numSeance() == 2) {
+        System.out.println(annotationsList[i].toString());
+      }
+    }
+  }
+
+  public void methodesDe2020NonFinalisees(miageAdvanced[] annotationsList) {
+    System.out.println("Liste des méthodes non finalisées et créées en 2020.");
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      // IL FAUT AJOUTER L ANNEEE
+      if (annotationsList[i].etatCompletude() == etat.DRAFT_PARTIEL) {
+        System.out.println(annotationsList[i].toString());
+      }
+    }
+  }
+
+  public void ratioMethodeFinalisesRealisees(miageAdvanced[] annotationsList, int anneeUniversitaire) {
+    System.out.println("Ratio entre les methodes finalisées et réalisées.");
+
+    ArrayList<miageAdvanced> methodesFinalises = new ArrayList<miageAdvanced>();
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      // IL FAUT AJOUTER L ANNEEE
+      if (annotationsList[i].etatCompletude() == etat.VERSION_FINALISEEE) {
+        methodesFinalises.add(annotationsList[i]);
+      }
+    }
+
+    System.out.println(methodesFinalises.size() / annotationsList.length);
+  }
+
+  public void ratioDraftParielMethodesRealisees(miageAdvanced[] annotationsList, int anneeUniversitaire) {
+    System.out.println("Ratio entre les methodes partiel et réalisées.");
+
+    ArrayList<miageAdvanced> methodesPartiel = new ArrayList<miageAdvanced>();
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      // IL FAUT AJOUTER L ANNEEE
+      if (annotationsList[i].etatCompletude() == etat.DRAFT_PARTIEL) {
+        methodesPartiel.add(annotationsList[i]);
+      }
+    }
+
+    System.out.println(methodesPartiel.size() / annotationsList.length);
+  }
+
+  public void ratioMethodesGenereesMethodesRealisees(miageAdvanced[] annotationsList, int anneeUniversitaire) {
+    System.out.println("Ratio entre les methodes génerées et réalisées.");
+
+    ArrayList<miageAdvanced> methodesGenereees = new ArrayList<miageAdvanced>();
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      // IL FAUT AJOUTER L ANNEEE
+      if (annotationsList[i].etatCompletude() == etat.DRAFT_PARTIEL) {
+        methodesGenereees.add(annotationsList[i]);
+      }
+    }
+
+    System.out.println(methodesGenereees.size() / annotationsList.length);
+  }
+
+  public void ratioMethodesFinaliseesMethodesNonGenerees(miageAdvanced[] annotationsList, int anneeUniversitaire) {
+    System.out.println("Ratio entre les methodes finalisées et non generees.");
+
+    ArrayList<miageAdvanced> methodesFinalisees = new ArrayList<miageAdvanced>();
+    ArrayList<miageAdvanced> methodesNonGenereees = new ArrayList<miageAdvanced>();
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      // IL FAUT AJOUTER L ANNEEE
+      if (annotationsList[i].etatCompletude() == etat.VERSION_FINALISEEE) {
+        methodesFinalisees.add(annotationsList[i]);
+      }
+      else if (annotationsList[i].etatCompletude() == etat.DRAFT_PARTIEL) {
+        methodesNonGenereees.add(annotationsList[i]);
+      }
+    }
+
+    System.out.println(methodesFinalisees.size() / methodesNonGenereees.size());
+  }
+
+  public void ratioMethodesTesteesMethodesNonGenerees(miageAdvanced[] annotationsList, int anneeUniversitaire) {
+    System.out.println("Ratio des méthodes testées / nombre de méthodes non générées");
+
+    ArrayList<miageAdvanced> methodesTestees = new ArrayList<miageAdvanced>();
+    ArrayList<miageAdvanced> methodesNonGenereees = new ArrayList<miageAdvanced>();
+
+    for (int i = 0; i < annotationsList.length; i++) {
+      // IL FAUT AJOUTER L ANNEEE
+      if (annotationsList[i].etatTests() == true) {
+        methodesTestees.add(annotationsList[i]);
+      }
+      else if (annotationsList[i].etatCompletude() == etat.DRAFT_PARTIEL) {
+        methodesNonGenereees.add(annotationsList[i]);
+      }
+    }
+
+    System.out.println(methodesTestees.size() / methodesNonGenereees.size());
+  }
+
+  // UTILS
+
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_PARTIEL, etatTests = false, etatAutomatisation = false)
   public static String litChaineAuClavier() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     return br.readLine();
   }
 
-  @miageBasics
+  @miageAdvanced(etatCompletude = miageAdvanced.etat.VERSION_FINALISEEE, etatQualite = miageAdvanced.etat.DRAFT_COMPLET, etatTests = false, etatAutomatisation = false)
   public void toString(Class cl) {
     Class[] innerClasses = cl.getDeclaredClasses();
 
@@ -279,13 +417,7 @@ public class AnalyseurDeClasse {
     }
   }
 
-  @miageBasics(
-    nom = "DELMARE",
-    prenom = "Thomas",
-    annee = 2021,
-    module = "Ui",
-    numSeance = 1
-  )
+  @miageBasics(nom = "DELMARE", prenom = "Thomas", annee = 2021, module = "Java avancée", numSeance = 1)
   public void toString(Class cl, int profondeur) {
     Class[] innerClasses = cl.getDeclaredClasses();
     int classesRestantes = profondeur;
@@ -296,22 +428,44 @@ public class AnalyseurDeClasse {
     }
   }
 
-  @miageBasics
+  // MAIN
+
+  @miageBasics(nom = "DELMARE", prenom = "Thomas", annee = 2021, module = "Java avancée", numSeance = 1)
   public static void main(String[] args) {
     boolean ok = false;
 
     while (!ok) {
-      try {
-        System.out.print("Entrez le nom d'une classe (ex : java.util.Date): ");
-        String nomClasse = litChaineAuClavier();
+      miageBasics[] currentBasicsAnnotations = AnalyseurDeClasse.class.getAnnotationsByType(miageBasics.class);
+      miageAdvanced[] currentAdvancedAnnotations = AnalyseurDeClasse.class.getAnnotationsByType(miageAdvanced.class);
 
-        analyseClasse(nomClasse);
-        ok = true;
-      } catch (ClassNotFoundException e) {
-        System.out.println("Classe non trouvée.");
-      } catch (IOException e) {
-        System.out.println("Erreur d'E/S!");
+      System.out.println(currentAdvancedAnnotations.length);
+      System.out.println(currentBasicsAnnotations.length);
+
+      for (int i = 0; i < currentBasicsAnnotations.length; i++) {
+        System.out.println(currentBasicsAnnotations[i].toString());
       }
+
+      for (int i = 0; i < currentAdvancedAnnotations.length; i++) {
+        System.out.println(currentAdvancedAnnotations[i].toString());
+      }
+
+      ok = true;
     }
+
+    // classSample.listerAnnotations(currentAnnotations);
+
+    // while (!ok) {
+    // try {
+    // // System.out.print("Entrez le nom d'une classe (ex : java.util.Date): ");
+    // // String nomClasse = litChaineAuClavier();
+
+    // // analyseClasse(nomClasse);
+    // ok = true;
+    // } catch (ClassNotFoundException e) {
+    // System.out.println("Classe non trouvée.");
+    // } catch (IOException e) {
+    // System.out.println("Erreur d'E/S!");
+    // }
+    // }
   }
 }
